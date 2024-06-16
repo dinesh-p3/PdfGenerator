496ae7206f826ada72a113b1ce8324375608da51
dk <dineshkumar.balaji@platform3solutions.com>
2024-06-16 19:19:26 +0530

    pdf util

--- src/main/java/com/generator/PdfGenerator/PdfGeneratorApplication.java
+++ src/main/java/com/generator/PdfGenerator/PdfGeneratorApplication.java
@@ -1,5 +1,6 @@
 package com.generator.PdfGenerator;
 
+import com.generator.PdfGenerator.util.PdfUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class PdfGeneratorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PdfGeneratorApplication.class, args);
+		PdfUtil.testPdf();
 	}
 
 }
